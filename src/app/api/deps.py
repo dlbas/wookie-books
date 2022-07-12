a2e75a448b416c12b7ddb2a7958e433141c1347f
Daniel Baskal <daniel.baskal@gmail.com>
2022-07-12 22:41:08 +0300

    add get current user

--- src/app/api/deps.py
+++ src/app/api/deps.py
@@ -5,11 +5,7 @@ from app.api.config import Settings
 from app.api.database import database_connection
 from app.models.user import User
 
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/token/")
-
-
-async def get_current_user(token: str = Depends(oauth2_scheme)):
-    return User(id=1, login="test", password="test", pseudonym="test", is_active=True)
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/login/")
 
 
 def token_secret_key() -> str:
--- src/app/services/auth.py
+++ src/app/services/auth.py
@@ -5,12 +5,15 @@ from fastapi import Depends, HTTPException, status
 from jose import jwt
 from passlib.context import CryptContext
 
-from app.api.deps import (database_connection, token_expires_in_minutes,
+from app.api.database import database_connection
+from app.api.deps import (oauth2_scheme, token_expires_in_minutes,
                           token_secret_key)
+from app.models.user import User
 from app.repositories.users import get_user_by_login, update_user_password
 from app.schemas.auth import LoginRequest, LoginResponse
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+JWT_ALG = "HS256"
 
 
 async def login_service(
@@ -19,17 +22,23 @@ async def login_service(
     token_expires_in: int = Depends(token_expires_in_minutes),
     secret: str = Depends(token_secret_key),
 ) -> LoginResponse:
+    """Login endpoint"""
     async with connection:
         await authenticate_user(
             connection, login=request.login, password=request.password
         )
-    token = create_token(expires_in=timedelta(minutes=token_expires_in), key=secret)
+    token = create_token(
+        expires_in=timedelta(minutes=token_expires_in),
+        username=request.login,
+        key=secret,
+    )
     return LoginResponse(token=token)
 
 
 async def authenticate_user(
     connection: Connection, *, login: str, password: str
 ) -> None:
+    """Authenticates user by login and password"""
     user = await get_user_by_login(connection, login=login)
     if not user:
         raise HTTPException(status_code=status.HTTP_403_FORBIDDEN)
@@ -40,15 +49,42 @@ async def authenticate_user(
         await update_user_password(
             connection, user_id=user.id, encrypted_password=hashed_password
         )
+        return
 
     if not verify_password(password, user.password):
         raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED)
+
+
+async def get_current_user(
+    token: str = Depends(oauth2_scheme),
+    secret: str = Depends(token_secret_key),
+    connection: Connection = Depends(database_connection),
+) -> User:
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Could not validate credentials",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
+    try:
+        payload = jwt.decode(token, secret, [JWT_ALG])
+    except jwt.JWTError:
+        raise credentials_exception
+
+    login = payload.get("sub")
+    if login is None:
+        raise credentials_exception
+
+    async with connection:
+        user = await get_user_by_login(connection, login=login)
+    if user is None:
+        raise credentials_exception
+    return user
 
 
 def verify_password(plain_password: str, hashed_password: str) -> bool:
     return pwd_context.verify(plain_password, hashed_password)
 
 
-def create_token(*, expires_in: timedelta, key: str) -> str:
-    data = {"exp": datetime.now(timezone.utc) + expires_in}
-    return jwt.encode(data, key, algorithm="HS256")
+def create_token(*, expires_in: timedelta, username: str, key: str) -> str:
+    data = {"exp": datetime.now(timezone.utc) + expires_in, "sub": username}
+    return jwt.encode(data, key, algorithm=JWT_ALG)
