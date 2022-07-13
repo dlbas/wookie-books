8023a47e5dbe93cf56200e13d22d1640bcb2b8c0
Daniel Baskal <daniel.baskal@gmail.com>
2022-07-13 20:42:14 +0300

    update migrations

--- contrib/data_migrations/0001-initial.sql
+++ contrib/data_migrations/0001-initial.sql
@@ -1,4 +1,8 @@
-insert into users (login, pseudonym, is_active) values
-                                                    ('john', 'Johny', true),
-                                                    ('jake', 'Jakie', true),
-                                                    ('dartvaider', 'iAmNotEvil666', false);
+insert into users (id, login, pseudonym, is_active)
+values (1, 'luke', 'Luke Skywalker', true),
+       (2, 'anakin', 'Anakin Skywalker', true),
+       (3, 'dartvaider', 'Dart Vaider', false);
+
+insert into books (id, title, description, cover_image_url, price, user_id)
+values (1, 'How I Met Yoda', 'The story on how I met Mr. Yoda', 'https://example.com/book1', 42, 1),
+       (2, 'How I Became Evil', 'Nuff said', 'https://example.com/book2', 24, 2);
--- contrib/migrations/0001-initial.sql
+++ contrib/migrations/0001-initial.sql
@@ -10,8 +10,11 @@ create table users
 
 create table books
 (
-    id          integer primary key,
-    title       varchar not null,
-    description varchar not null,
-    user_id     integer references users (id)
+    id              integer primary key,
+    title           varchar                  not null,
+    description     varchar                  not null,
+    cover_image_url varchar                  not null,
+    price           decimal                  not null,
+    unpublished_at  timestamp with time zone null,
+    user_id         integer references users (id)
 );
